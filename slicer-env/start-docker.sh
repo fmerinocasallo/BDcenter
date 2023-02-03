475ea32fd43ae54b6d94f8fdde6f6ffff3dff9a9
gabnasello <gabriele.nasello@gmail.com>
2023-02-03 14:31:34 +0100

    add Jupyter Lab

--- slicer-env/start-docker.sh
+++ slicer-env/start-docker.sh
@@ -6,6 +6,10 @@ SERVICENAME='slicer'
 GUIPORT=`python3 -c "import socket; s=socket.socket(); s.bind(('',0)); print(s.getsockname()[1]); s.close()"`
 export GUIPORT
 
+# Get a free port in the computer
+JLPORT=`python3 -c "import socket; s=socket.socket(); s.bind(('',0)); print(s.getsockname()[1]); s.close()"`
+export JLPORT
+
 #HOST_NAME=${HOSTNAME}
 HOST_NAME='localhost'
 export HOST_NAME
