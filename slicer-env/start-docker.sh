57bcda923459bc99b5a93224b2fde62b0f2c44d8
gabnasello <gabriele.nasello@gmail.com>
2023-02-08 15:06:01 +0100

    make slice-env user specific

--- slicer-env/start-docker.sh
+++ slicer-env/start-docker.sh
@@ -13,6 +13,10 @@ export JLPORT
 #HOST_NAME=${HOSTNAME}
 HOST_NAME='localhost'
 export HOST_NAME
+RES_UID=$UID
+export RES_UID
+RES_GID=$(id -g)
+export RES_GID
 
 envsubst < env_usertemplate > .env
 
