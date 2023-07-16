10917417f94b826a0de667db7412282a65e329ac
gabnasello <gabriele.nasello@gmail.com>
2023-07-16 17:43:09 -0400

    add docker.deamon

--- datascience-env/pull_singularity.sh
+++ datascience-env/pull_singularity.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-VERSION='2023-04-20'
+VERSION='2023-07-16 '
 DOCKERIMAGE='gnasello/datascience-env'
 
 IMAGENAME='datascience-env.sif'
 
 singularity pull --name $IMAGENAME docker://$DOCKERIMAGE:$VERSION
+#singularity pull --name $IMAGENAME docker-daemon:$DOCKERIMAGE:$VERSION
