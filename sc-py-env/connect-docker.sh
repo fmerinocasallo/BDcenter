0bac95d2a3f421bac22ff4441da176cbd1163f3c
gabnasello <gabriele.nasello@gmail.com>
2023-07-16 20:27:16 -0400

    remove image3D and sc-py to sing

--- sc-py-env/connect-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SERVICENAME='sc-py-env'
-
-docker compose exec $SERVICENAME bash
---
+++ sc-py-env/start_singularity.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+IMAGENAME='sc-py-env.sif'
+export IMAGENAME
+INSTANCENAME='sc-py-env_'$USER
+
+singularity instance start $IMAGENAME $INSTANCENAME
+
+singularity exec instance://$INSTANCENAME /bin/bash entrypoint.sh
+
+singularity shell instance://$INSTANCENAME
+
+singularity instance stop $INSTANCENAME
