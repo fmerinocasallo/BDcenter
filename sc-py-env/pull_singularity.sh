cbbc3bf6d0498fb6243d12f45d78508900ad67bc
gabnasello <gabriele.nasello@gmail.com>
2023-10-15 23:59:03 +0200

    switch to linuxserver base image

--- sc-py-env/pull_singularity.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VERSION='latest'
-DOCKERIMAGE='gnasello/sc-py-env'
-
-IMAGENAME='sc-py-env.sif'
-
-singularity pull --name $IMAGENAME docker://$DOCKERIMAGE:$VERSION
-#singularity pull --name $IMAGENAME docker-daemon:$DOCKERIMAGE:$VERSION
