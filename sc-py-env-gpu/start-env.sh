0b5da0827b8c3723d3243cdc23e30336d48ad92b
gabnasello <gabriele.nasello@gmail.com>
2024-05-15 14:59:17 +0200

    add sc-py gpu

---
+++ sc-py-env-gpu/start-env.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SERVICENAME='sc-py-env-gpu'
+export SERVICENAME
+
+COMPOSE_PROJECT_NAME=$USER-$SERVICENAME
+export COMPOSE_PROJECT_NAME
+
+# Get a free port in the computer
+JLPORT=`python3 -c "import socket; s=socket.socket(); s.bind(('',0)); print(s.getsockname()[1]); s.close()"`
+export JLPORT
+
+HOST_NAME=${HOSTNAME}
+#HOST_NAME='localhost'
+export HOST_NAME
+RES_UID=$(id -u)
+export RES_UID
+RES_GID=$(id -g)
+export RES_GID
+
+envsubst < env_usertemplate > .env
+
+docker compose up -d
+
+#sleep 5
+
+bash ./entrypoint.sh
+
+# Print container version
+docker compose exec $SERVICENAME bash -c 'echo "$DOCKER_IMAGE_NAME:$VERSION";echo' 
+
+docker compose exec --user jovyan $SERVICENAME bash
+
+docker compose down
