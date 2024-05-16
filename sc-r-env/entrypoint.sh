0195d4b12878487a80cb298e1ac552f468432a86
gabnasello <gabriele.nasello@gmail.com>
2024-05-16 15:38:45 +0200

    switch sc-r-env to docker

--- sc-r-env/entrypoint.sh
+++ sc-r-env/entrypoint.sh
@@ -2,21 +2,18 @@
 
 ADDRESS=${HOSTNAME} #127.0.0.1
 
-JLPORT=`python3 -c "import socket; s=socket.socket(); s.bind(('',0)); print(s.getsockname()[1]); s.close()"` #7878
-
 BOLDGREEN='\033[1;32m'
 GREEN='\033[0;32m'
 NOCOLOR='\033[0m' # No Color
 
-echo -e '\n '$BOLDGREEN $IMAGENAME $NOCOLOR' \n'
+echo -e '\n '$BOLDGREEN $SERVICENAME $NOCOLOR' \n'
 
-nohup jupyter lab --no-browser --notebook-dir=$HOME --ip=0.0.0.0 --port=$JLPORT  > jupyter_server_output.out 2>&1 &
+echo -e '\n List of servers available in this container\n'
 
-echo -e $GREEN'\n   JupyterLab \n'$NOCOLOR
+echo -e '\n '$GREEN JupyerLab $NOCOLOR' \n'
 
-sleep 2
+#docker compose exec --user jovyan $SERVICENAME jupyter server list
 
-jupyter server list
-#sed -n 54p jupyter_server_output.out
+echo -e '\n If no container is listed, run the command `jl` in the terminal. \n It might need a few seconds to startup the server. \n'
 
 echo -e '\n Press `CTRL + D` To close the container \n'
--- sc-r-env/pull_singularity.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VERSION='latest'
-DOCKERIMAGE='gnasello/sc-r-env'
-
-IMAGENAME='sc-r-env.sif'
-
-singularity pull --name $IMAGENAME docker://$DOCKERIMAGE:$VERSION
-#singularity pull --name $IMAGENAME docker-daemon:$DOCKERIMAGE:$VERSION
